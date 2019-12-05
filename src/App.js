bb5d0009a72e7e2e06ede7ae0d279e5661ace1eb
zackartz <zach@zacharymyers.com>
2019-12-04 14:21:37 -1000

    fix app hopefully

--- src/App.js
+++ src/App.js
@@ -156,10 +156,10 @@ class App extends Component {
         	<div className="App">
 				<AppBar position="static">
 				  <Toolbar>
-				    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+				    <IconButton edge="start" color="inherit" aria-label="menu">
 				      <MenuIcon />
 				    </IconButton>
-				    <Typography variant="h6" className={classes.title}>
+				    <Typography variant="h6">
 				      News
 				    </Typography>
 				    <Button color="inherit">Login</Button>
